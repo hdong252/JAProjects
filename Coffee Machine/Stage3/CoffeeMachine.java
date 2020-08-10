19f0d851d047f3eecc3ffc22b663da8a0bb47fe8
Huilun <hd252@cornell.edu>
2020-08-10 13:46:25 -0700

    removing unnecessary comments

--- Coffee Machine/Stage3/CoffeeMachine.java
+++ Coffee Machine/Stage3/CoffeeMachine.java
@@ -3,16 +3,7 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        /* //Stage 1
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!"); */
         
-        // System.out.println("Write how many cups of coffee you will need: ");
         Scanner s = new Scanner(System.in);
         int waterPerCup = 200;
         int milkPerCup = 50;
@@ -40,14 +31,6 @@ public class CoffeeMachine {
         } else {
             System.out.println("Yes, I can make that amount of coffee (and even " + (cupsCanMake - cupsNeeded) + " more than that)");
         }
-        
-        /* //Stage 2
-        int cupsOfCoffee = s.nextInt();
-        
-        System.out.println("For 25 cups of coffee you will need:");
-        System.out.println(200 * cupsOfCoffee + " ml of water");
-        System.out.println(50 * cupsOfCoffee + " ml of milk");
-        System.out.println(15 * cupsOfCoffee + " g of coffee beans"); */
         
     }
 }
